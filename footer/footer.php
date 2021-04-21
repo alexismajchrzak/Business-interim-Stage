fbae396aef099dae06c997f802c991e95ecfdaa9
alexismajchrzak <alexis.majchrzak77@gmail.com>
2021-04-21 14:54:31 +0200

    ajout footer

---
+++ footer/footer.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Prenium Interim</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+  <link href="footer.css" rel="stylesheet" />
+
+</head>
+
+<body>
+
+<footer class="container-fluid bg-grey py-5">
+<div class="container">
+   <div class="row">
+
+      <div class="col-md-6">
+         <div class="row">
+
+            <div class="col-md-6 ">
+               <div class="logo-part">
+                  <h1 class="w-50 logo-footer" > Prenium Interim </h1>
+                  <p>17 rue de l'Etang - 95943 ROISSY CHARLES DE GAULLE</p>
+                  <p>Mettre un petit slogan </p>   <!-- mettre un petit slogan ici -->
+               </div>
+            </div>
+
+            <div class="col-md-6 px-4">
+               <h6> À propos </h6>
+               <p> possibilité de  texte a mettre .</p>  <!-- texte a mettre -->
+               <a href="#" class="btn-footer"> Plus d'infos   </a><br>
+               <a href="#" class="btn-footer"> Nous contacter </a>
+            </div>
+
+         </div>
+      </div>
+
+      <div class="col-md-6">
+         <div class="row">
+            <div class="col-md-6 px-4">
+
+               <h6> Help us</h6>
+               <div class="row ">
+
+                  <div class="col-md-6">
+                     <ul>
+                        <li> <a href="#"> Home</a> </li>
+                        <li> <a href="#"> About</a> </li>
+                        <li> <a href="#"> Service</a> </li>
+                        <li> <a href="#"> Team</a> </li>
+                        <li> <a href="#"> Help</a> </li>
+                        <li> <a href="#"> Contact</a> </li>
+                     </ul>
+                  </div>
+
+                  <div class="col-md-6 px-4">
+                     <ul>
+                        <li> <a href="#"> Cab Faciliy</a> </li>
+                        <li> <a href="#"> Fax</a> </li>
+                        <li> <a href="#"> Terms</a> </li>
+                        <li> <a href="#"> Policy</a> </li>
+                        <li> <a href="#"> Refunds</a> </li>
+                        <li> <a href="#"> Paypal</a> </li>
+                     </ul>
+                  </div>
+
+               </div>
+            </div>
+
+            <div class="col-md-6 ">
+
+               <h6> Réseaux </h6>
+
+               <div class="social">
+                  <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+
+               </div>
+               
+               <p class="reseaux" >Venez faire un tour sur nos réseaux </p>
+            </div>
+
+         </div>
+      </div>
+
+   </div>
+</div>
+</div>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+</body>
+</html>
