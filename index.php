7377d5d450f39230fda3553c174e8895b766bccd
alexismajchrzak <alexis.majchrzak77@gmail.com>
2021-05-01 20:08:14 +0200

    ajout "counter" index

--- index.php
+++ index.php
@@ -154,6 +154,50 @@
 <!-- fin expertise -->
 
 
+
+
+<div id="projectFacts" class="sectionClass"  >
+    <div class="fullWidth eight columns">
+        <div class="projectFactsWrap ">
+
+            <div class="item wow fadeInUpBig animated animated" data-number="12" style="visibility: visible;">
+                <i class="fa fa-briefcase"></i>
+                <p id="number1" class="number">12</p>
+                <span></span>
+                <p>Projects done</p>
+            </div>
+
+            <div class="item wow fadeInUpBig animated animated" data-number="55" style="visibility: visible;">
+                <i class="fa fa-smile-o"></i>
+                <p id="number2" class="number">55</p>
+                <span></span>
+                <p>Happy clients</p>
+            </div>
+
+            <div class="item wow fadeInUpBig animated animated" data-number="359" style="visibility: visible;">
+                <i class="fa fa-coffee"></i>
+                <p id="number3" class="number">359</p>
+                <span></span>
+                <p>Cups of coffee</p>
+            </div>
+
+            <div class="item wow fadeInUpBig animated animated" data-number="246" style="visibility: visible;">
+                <i class="fa fa-camera"></i>
+                <p id="number4" class="number">246</p>
+                <span></span>
+                <p>Photos taken</p>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
 <?php
         include "footer/footer.php"
     ?>
